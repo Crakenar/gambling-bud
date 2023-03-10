7dc746255cd147801b8dd527e0865d2d6846442e
Crakenar <teothou@hotmail.fr>
2023-03-10 07:43:10 +0100

    casse les couilles en fait

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected : ${conn.connection.host}`)
+  // const conn = await mongoose.connect('mongodb://mongo:27017/gambling_db', {
+  const conn = await mongoose.connect('mongodb://localhost:27017/gambling_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })  .then(() => console.log(`MongoDB connected`))
+      .catch(err => console.log(err));
 };
 
 module.exports = connectDB;
