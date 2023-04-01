463036cbe30cd3a81009a92bc9e5c2224d478eb5
Crakenar <teothou@hotmail.fr>
2023-04-01 23:00:22 +0200

    adding iconify

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,11 @@
 <template>
   <TabsDashboard :defaultTab="tab" @change-tab="changeTab"/>
-  <Dashboard v-if="tab === TABS.DASHBOARD"/>
-  <StatsDashboard v-if="tab === TABS.STATS" />
+  <keep-alive>
+    <Dashboard v-if="tab === TABS.DASHBOARD"/>
+  </keep-alive>
+  <keep-alive>
+    <StatsDashboard v-if="tab === TABS.STATS"/>
+  </keep-alive>
 </template>
 
 <script setup lang="ts">
@@ -10,10 +14,11 @@ import TabsDashboard from "@/components/Dashboard/TabsDashboard.vue";
 import StatsDashboard from "@/components/Dashboard/Statistics/StatsDashboard.vue";
 import {useDashboardStore} from "@/stores/dashBoardStore";
 import {storeToRefs} from "pinia";
+import {Icon} from "@iconify/vue";
 
 const dashboardStore = useDashboardStore();
-const { tab } = storeToRefs(dashboardStore);
-const { setTab } = dashboardStore
+const {tab} = storeToRefs(dashboardStore);
+const {setTab} = dashboardStore
 const TABS = {
   DASHBOARD: 0,
   STATS: 1
