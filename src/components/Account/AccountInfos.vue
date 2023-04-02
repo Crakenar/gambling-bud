aa88ddd4d80a8a78732d393328a88274ee85e58d
Crakenar <teothou@hotmail.fr>
2023-04-02 15:34:34 +0200

    Add : basic profile information

--- src/components/Account/AccountInfos.vue
+++ src/components/Account/AccountInfos.vue
@@ -3,6 +3,26 @@
     <h3 class="border-b h-16 flex justify-center items-center">
       Profile Information
     </h3>
+    <div class="p-4">
+      <div class="flex justify-between">
+        <strong>Full Name</strong>
+        <p>{{ getDisplayName() }}</p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+    <div class="p-4">
+      <div class="flex justify-between">
+        <strong>Email</strong> <p>{{ user.email }}</p>
+      </div>
+    </div>
+    <div class="p-4">
+      <div class="flex justify-between">
+        <strong>Balance</strong> <p>{{ user.balance }}</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -13,6 +33,7 @@ import {storeToRefs} from "pinia";
 
 const userStore = useUserStore();
 const {user} = storeToRefs(userStore);
+const {getDisplayName} = userStore;
 </script>
 
 <style scoped>
--- src/components/Account/FriendItem.vue
+++ src/components/Account/FriendItem.vue
@@ -6,7 +6,7 @@
     </div>
     <!-- right navbar -->
     <div class="flex items-center relative">
-      <img v-if="props.user.profileImage" :src="props.user.profileImage" referrerpolicy="no-referrer" class="ml-4 w-14 h-14 border-4 rounded-full shadow-lg" alt="profile picture">
+      <img v-if="props.user.profileImage" :src="props.user.profileImage" referrerpolicy="no-referrer" class="ml-4 w-14 h-14 ring-4 ring-gray-300 rounded-full shadow-lg" alt="profile picture">
       <Icon v-else icon="mdi:account-circle-outline" width="60"/>
     </div>
   </div>
