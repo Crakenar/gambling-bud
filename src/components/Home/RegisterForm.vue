d4d8e333d281e0b3ac584bee52a2b91c2cd7c0f9
Crakenar <teothou@hotmail.fr>
2023-04-02 21:24:00 +0200

    loader + switch tab

--- src/components/Home/RegisterForm.vue
+++ src/components/Home/RegisterForm.vue
@@ -1,51 +1,53 @@
 <template>
-  <transition name="fade">
-    <div class="space-y-4 md:space-y-6" action="#">
-      <div>
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-        <input v-model="registerForm.email" type="email" name="email" id="email"
-               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-               placeholder="name@company.com" required="">
-      </div>
-      <div>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-        <input @input="checkPassword" v-model="registerForm.password" type="password" name="password" id="password"
-               placeholder="••••••••"
-               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-               required="">
-      </div>
-      <div>
-        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
-          password</label>
-        <input v-model="registerForm.confirmPassword" type="confirm-password" name="confirm-password"
-               id="confirm-password" placeholder="••••••••"
-               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-               required="">
-      </div>
-      <!--                <div class="flex items-start">-->
-      <!--                  <div class="flex items-center h-5">-->
-      <!--                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">-->
-      <!--                  </div>-->
-      <!--                  <div class="ml-3 text-sm">-->
-      <!--                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>-->
-      <!--                  </div>-->
-      <!--                </div>-->
-      <button
-          :disabled="!validPassword"
-          @click="register"
-          type="submit"
-          class="inline-block w-full rounded bg-primary px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal
+  <LoadingPage :loading="loading" :is-component="false">
+    <transition name="fade">
+      <div class="space-y-4 md:space-y-6" action="#">
+        <div>
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input v-model="registerForm.email" type="email" name="email" id="email"
+                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 placeholder="name@company.com" required="">
+        </div>
+        <div>
+          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+          <input @input="checkPassword" v-model="registerForm.password" type="password" name="password" id="password"
+                 placeholder="••••••••"
+                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 required="">
+        </div>
+        <div>
+          <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+            password</label>
+          <input v-model="registerForm.confirmPassword" type="confirm-password" name="confirm-password"
+                 id="confirm-password" placeholder="••••••••"
+                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 required="">
+        </div>
+        <!--                <div class="flex items-start">-->
+        <!--                  <div class="flex items-center h-5">-->
+        <!--                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">-->
+        <!--                  </div>-->
+        <!--                  <div class="ml-3 text-sm">-->
+        <!--                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>-->
+        <!--                  </div>-->
+        <!--                </div>-->
+        <button
+            :disabled="!validPassword"
+            @click="register"
+            type="submit"
+            class="inline-block w-full rounded bg-primary px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal
                    text-white shadow-[0_10px_18px_8px_#3b71ca]
                     focus:outline-none focus:ring-0 active:bg-primary-700"
-          data-te-ripple-init
-          data-te-ripple-color="light">
-        Create account
-      </button>
-      <!--                <p class="text-sm font-light text-gray-500 dark:text-gray-400">-->
-      <!--                  Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>-->
-      <!--                </p>-->
-    </div>
-  </transition>
+            data-te-ripple-init
+            data-te-ripple-color="light">
+          Create account
+        </button>
+        <!--                <p class="text-sm font-light text-gray-500 dark:text-gray-400">-->
+        <!--                  Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>-->
+        <!--                </p>-->
+      </div>
+    </transition>
+  </LoadingPage>
   <Snackbar v-if="accountRegistered">
       <p class="text-green-700">Account Registered</p>
   </Snackbar>
@@ -57,6 +59,7 @@ import AuthService from "@/services/AuthService";
 import type {registerInterface} from "@/Interface/AuthInterface";
 import router from "@/router";
 import Snackbar from "@/components/Atom/Snackbar.vue";
+import LoadingPage from "@/components/Widgets/LoadingPage.vue";
 
 const passwordLength = ref<number>(0);
 const containsEightCharacters = ref<boolean>(false);
@@ -64,7 +67,7 @@ const containsNumber = ref<boolean>(false);
 const containsUppercase = ref<boolean>(false);
 const containsSpecialCharacter = ref<boolean>(false);
 const validPassword = ref<boolean>(false);
-
+const loading = ref<boolean>(false)
 const accountRegistered = ref<boolean>(false);
 
 const registerForm = ref<registerInterface>({
@@ -73,6 +76,9 @@ const registerForm = ref<registerInterface>({
   confirmPassword: '',
 });
 
+const emit = defineEmits<{
+  (e: 'switchTab') :void
+}>();
 
 const checkPassword = () => {
   passwordLength.value = registerForm.value.password.length;
@@ -92,16 +98,20 @@ const checkPassword = () => {
       // samePassword.value === true;
 }
 
-const register = () => {
+const register = async () => {
   if (validPassword.value) {
-    AuthService.register(registerForm.value).then(res => {
+    loading.value = true;
+   await AuthService.register(registerForm.value).then(res => {
       if (res.data.success) {
         //TODO => make popup good
         accountRegistered.value = res.data.success
         // router.replace({ path: '/dashboard' })
+        emit('switchTab');
       }
+      loading.value = false;
     }).catch(e => {
       console.log('erreur register', e);
+      loading.value = false;
     });
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,7 @@
               </ul>
             </div>
             <LoginForm v-if="selectedTab === TabsEnum.LOGIN.index" />
-            <RegisterForm v-if="selectedTab === TabsEnum.REGISTER.index" />
+            <RegisterForm v-if="selectedTab === TabsEnum.REGISTER.index" @switch-tab="changeTab(TabsEnum.LOGIN.index)" />
             <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t
                   before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
               <p class="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
